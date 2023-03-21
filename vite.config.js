573ab66769ebe4ab12a44eb6175466de73fc18fd
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2023-03-21 21:48:49 +0100

    Trying to build, sass url() not working

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+
+export default {
+    // config options
+    build: {
+        outDir: "_preview-wip",
+
+
+    },
+    css:
+    {
+        devSourcemap: true,
+    },
+    assetsInclude: ['/img'],
+
+
+}
