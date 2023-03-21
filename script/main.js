1ff7b923d4457477aac24725c6b5cccb1bd454de
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2023-03-21 23:09:14 +0100

    Temporary solution for build background-image url()

--- script/main.js
+++ script/main.js
@@ -1,2 +1,4 @@
 import '../sass/main.scss';
+import testImg from '../img/01_header-fabio-comparelli-uq2E2V4LhCY-unsplash.jpg';
+document.getElementById('hbg').style.backgroundImage = `url(${testImg})`;
 
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default {
     {
         devSourcemap: true,
     },
+
     assetsInclude: ['/img'],
 
 
