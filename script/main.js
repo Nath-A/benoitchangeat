386445a33fa60c1e8505a4524b0f8774af3e9edf
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2023-03-22 15:27:49 +0100

    last build

--- script/main.js
+++ script/main.js
@@ -2,5 +2,5 @@ import '../sass/main.scss';
 
 //JS hack to resolve bad import from vite
 import testImg from '../img/01_header-fabio-comparelli-uq2E2V4LhCY-unsplash.jpg';
-document.querySelector('header__background-image').style.backgroundImage = `url(${testImg})`;
+document.querySelector('.header__background-image').style.backgroundImage = `url(${testImg})`;
 
