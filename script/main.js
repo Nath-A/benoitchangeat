2ead08c54ffa03025568e348c8b61a7979bfaf83
Nath A <nathalie.anneessens@gmail.com>
2023-06-13 20:01:39 +0200

    newsletter subscription : wip

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 import '../sass/main.scss';
 
+import '../script/mailchimpScripts';
 // //JS hack to resolve bad import from vite
 // import testImg from '../img/01_header-fabio-comparelli-uq2E2V4LhCY-unsplash.jpg';
 // document.querySelector('.header__background-image').style.backgroundImage = `url(${testImg})`;
